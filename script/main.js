43f6091187cef973dda9b3350ccc7318ed13e74c
NewtonGitau <newtongitau.nm@gmail.com>
2021-12-13 23:22:06 +0300

    Add a brief description in heading.

--- script/main.js
+++ script/main.js
@@ -2,35 +2,47 @@ var day=document.getElementById("day")
 var month=document.getElementById("month")
 var year=document.getElementById("year")
 var result=document.getElementById("result")
-var sex=document.getElementById("sex")
+var male=document.getElementById("male")
+var female=document.getElementById("female")
 
-function calculate()
-{
-    var dates=new Date(day.value, month.value, year.value);
-    var femaleDays=dates.getDay()
-    var maleDays=dates.getDay();
 
-    if(value="male") {
-        console.log(); switch (maleDays)
-    
-        {   case 0:result.innerHTML="Kwasi"; break;
-            case 1:result.innerHTML="Kwadwo"; break;
-            case 2:result.innerHTML="Kwabena"; break;
-            case 3:result.innerHTML="Kwaku"; break;
-            case 4:result.innerHTML="Yaw"; break;
-            case 5:result.innerHTML="Kofi"; break;
-            case 6:result.innerHTML="Kwame"; break;
-        }
-    } else if(value="female"){
-        console.log(); switch (femaleDays)
-        {
-            case 0:result.innerHTML="Akosua"; break;
-            case 1:result.innerHTML="Adwoa"; break;
-            case 2:result.innerHTML="Abenaa"; break;
-            case 3:result.innerHTML="Akua"; break;
-            case 4:result.innerHTML="Yaa"; break;
-            case 5:result.innerHTML="Afua"; break;
-            case 6:result.innerHTML="Ama"; break;
+function validateForm() {
+    let x = document.forms["myForm"]["fname"].value;
+    if(day<0 || day>31)
+        alert("Invalid day!")
+      return false;
+      if(month<0 || month>12)
+        alert("Invalid month!")
+        return false;
+    }
+  }
+
+    function calculate()
+    {
+        var dates=new Date(day.value, month.value, year.value);
+        
+        if(gender="male") {
+            console.log(); switch (maleDays)
+        
+            {   case 0:result.innerHTML="Kwasi"; break;
+                case 1:result.innerHTML="Kwadwo"; break;
+                case 2:result.innerHTML="Kwabena"; break;
+                case 3:result.innerHTML="Kwaku"; break;
+                case 4:result.innerHTML="Yaw"; break;
+                case 5:result.innerHTML="Kofi"; break;
+                case 6:result.innerHTML="Kwame"; break;
+            }
+        } else if(gender="female"){
+            console.log(); 
+            switch (femaleDays)
+            {
+                case 0:result.innerHTML="Akosua"; break;
+                case 1:result.innerHTML="Adwoa"; break;
+                case 2:result.innerHTML="Abenaa"; break;
+                case 3:result.innerHTML="Akua"; break;
+                case 4:result.innerHTML="Yaa"; break;
+                case 5:result.innerHTML="Afua"; break;
+                case 6:result.innerHTML="Ama"; break;
+            }
         }
     }
-}
